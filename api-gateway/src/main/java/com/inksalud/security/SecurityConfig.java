76f3007d03ed2418ba7c2f099d417b95aed112ed
elMarce2007 <alejandromarcelolicueva@gmail.com>
2026-02-15 10:46:57 -0500

    Add files via upload

---
+++ api-gateway/src/main/java/com/inksalud/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.inksalud.security;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	                .requestMatchers("/auth/**").permitAll() 
+	                .requestMatchers("/error").permitAll()    
+	                .anyRequest().authenticated()            
+	        )
+	        .sessionManagement(session -> 
+	            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        )
+	        .oauth2ResourceServer(oauth2 ->
+	                oauth2.jwt(jwt -> jwt.decoder(jwtDecoder()))
+	        );
+
+	    return http.build();
+	}
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        String secret = "inkasalud-super-secret-key-2026-123456";
+
+        SecretKey secretKey = new SecretKeySpec(
+        		secret.getBytes(java.nio.charset.StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+}
